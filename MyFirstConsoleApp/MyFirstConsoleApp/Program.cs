01119fbd04b03954e89c58d235d45089e8b385b6
s26904 <s26904@pjwstk.edu.pl>
2024-03-19 01:16:15 +0100

    In this commit I did the code for the Task 4
    Starting to do the merge using rebase.

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -8,20 +8,19 @@ class Program
 {
     private static void Main(string[] args)
     {
-        int[] arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 288 ];
+        int[] arr = [ 1, 2, 3, 4, 5, 666, 7, 8, 288 ];
         var arrAvg = CalculateAvg(arr);
         Console.WriteLine("The average for the array: [" + string.Join(", ", arr) + "]\nIs a number: " + arrAvg);
     }
 
     private static int CalculateAvg(int[] arr)
     {
-        var totalCount = 0;
-        var totalSum = 0;
+        var maxValue = arr[0];
         foreach (var num in arr)
         {
-            totalSum += num;
-            totalCount++;
+            if (maxValue <= num) maxValue = num;
         }
-        return totalSum / totalCount;
+
+        return maxValue;
     }
 }
